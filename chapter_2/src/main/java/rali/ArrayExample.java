765dfdae4a5ad57c6a1664f08935f649c226913d
Petr Arsentev <arsentyev.petr@otr.ru>
2016-03-21 14:30:37 +0300

    init commit

---
+++ chapter_2/src/main/java/rali/ArrayExample.java
@@ -0,0 +1,27 @@
+package rali;
+
+public class ArrayExample {
+	public static void main(String[] args) {
+		User[] users = new User[5];
+		users[0] = new User("petr");
+		users[1] = new User("nick");
+		users[2] = new User("make");
+		users[3] = new User("miron");
+		users[4] = new User("ban");
+	
+		User user = ArrayExample.findByName(users, "nick");
+
+		System.out.println(user.name);
+	}
+	
+	public static User findByName(User[] users, String name) {
+		User result = null;
+		for (User user : users) {
+			if (name.equals(user.name)) {
+				result = user;
+				break;
+			}
+		}
+		return result;
+	}
+}
---
+++ chapter_8/src/main/java/rali/User.java
@@ -0,0 +1,16 @@
+package rali;
+
+public class User {
+	public String name;
+
+	public User() {
+	}
+
+	public User(String name) {
+		this.name = name;	
+	}
+
+	public String getName() {
+		return this.name;
+	}
+}
